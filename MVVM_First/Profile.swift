6d867aa6ff472f2321335ad8ab15e29eb9d33897
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-08-26 22:18:31 +0300

    MVC - 1 (Preparation for MVVM)

---
+++ MVVM_First/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  MVVM_First
+//
+//  Created by Павел Кузин on 26.08.2020.
+//  Copyright © 2020 Павел Кузин. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    
+    var name        : String
+    var secondName  : String
+    var age         : Int
+}
