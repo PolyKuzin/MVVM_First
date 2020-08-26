fd95381a9824068a1767e153b80e0bfd13f9d865
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-08-26 18:30:10 +0300

    Initial Commit

---
+++ MVVM_First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_First
+//
+//  Created by Павел Кузин on 26.08.2020.
+//  Copyright © 2020 Павел Кузин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
